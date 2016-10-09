10c3217945c39c69ab12d3e28ba19e5ef32322a0
MythlinMasterdo <miles71397@gmail.com>
2016-10-08 20:15:50 -0700

    hell

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,39 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({results: [{'username': 'Miles', 'roomname': '', objectId: '1', 'text': 'Hello world!'}]}));
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      console.log('request body ', req.body);
+
+      models.messages.post(req.body, function(err) {
+        console.log('Controller error ', err);
+
+        if (err) {
+          console.log('Error in controllers messages post');
+          throw err;
+        } else {
+          res.writeHead(201, {'Content-Type': 'application/json'});
+        }
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {},
-    post: function (req, res) {}
+    post: function (req, res) {
+      models.users.post(req.body, function(err) {
+        if (err) {
+          console.log('Error in controllers users post');
+          throw err;
+        }
+        res.writeHead(201, {'Content-Type': 'application/json'});
+      });
+    }
   }
 };
+
 
--- server/models/index.js
+++ server/models/index.js
@@ -3,13 +3,26 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (object, callback) {
+      var queryString = 'INSERT INTO messages(userid, text) VALUES(?, ?)';
+
+      db.query(queryString, [object.id, object.text], function(err, rows, field) {
+        callback(err, rows);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function () {},
-    post: function () {}
+    post: function (object, callback) {
+      var queryString = 'INSERT INTO users(username) VALUES(?)';
+
+      db.query(queryString, [object.username], function(err, rows, field) {
+        callback(rows);
+      });
+    }
   }
 };
 
+// INSERT INTO tbl_name (a,b,c) VALUES(1,2,3),(4,5,6),(7,8,9);
